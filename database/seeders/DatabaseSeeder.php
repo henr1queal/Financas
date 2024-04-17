8fd6337a6fed53255398146bebc1e982bc523521
Henrique Silva <henriquersilva.al@gmail.com>
2024-04-16 21:19:05 -0300

    feat: user seed to two default users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,20 @@ class DatabaseSeeder extends Seeder
         ]);
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Usuario 1',
+            'email' => 'usuario1@gmail.com',
+            'password' => Hash::make('password'),
+            'wallet_id' => 1,
+            'created_at' => now(),
+        ]);
+        
+        \App\Models\User::factory()->create([
+            'name' => 'Usuario 2',
+            'email' => 'usuario2@gmail.com',
+            'password' => Hash::make('password'),
+            'wallet_id' => 1,
+            'created_at' => now(),
+        ]);
     }
 }
